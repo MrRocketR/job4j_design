b7808c11427151d29229bb7842f12357ab2c1042
MrRocketR <kuptovns@gmail.com>
2022-02-27 20:32:46 +0300

    SimpleLinkedList

---
+++ src/main/java/ru/job4j/collections/List.java
@@ -0,0 +1,6 @@
+package ru.job4j.collections;
+
+public interface List<E> extends Iterable<E> {
+    void add(E value);
+    E get(int index);
+}
---
+++ src/main/java/ru/job4j/collections/SimpleLinkedList.java
@@ -0,0 +1,22 @@
+package ru.job4j.collections;
+
+import ru.job4j.generics.Node;
+
+import java.util.Iterator;
+
+public class SimpleLinkedList<E> implements List<E> {
+    @Override
+    public void add(E value) {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
