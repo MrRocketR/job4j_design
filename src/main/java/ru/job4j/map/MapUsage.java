4ed400358878de756c21cecfef6c9ce7cb4fbc3f
MrRocketR <kuptovns@gmail.com>
2022-03-16 01:52:29 +0300

    Map

--- src/main/java/ru/job4j/map/MapUsage.java
+++ src/main/java/ru/job4j/map/MapUsage.java
@@ -10,14 +10,14 @@ public class MapUsage {
     public static void main(String[] args) {
         Calendar c = new GregorianCalendar(1990, Calendar.APRIL, 21);
         String s = "Ivan";
-        User user1 = new User(s, 2, c);
-        User user2 = new User(s, 2, c);
+        int i = 2;
+        User user1 = new User(s, i, c);
+        User user2 = new User(s, i, c);
         Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
         map.put(user2, new Object());
+        System.out.println(user1.hashCode());
+        System.out.println(user2.hashCode());
         System.out.println(map.entrySet());
-        System.out.println(user1.equals(user2));
-        System.out.println(map.get(user1).hashCode());
-        System.out.println(map.get(user2).hashCode());
     }
 }
