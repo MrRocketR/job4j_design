6e38007b15e3eace13d81be7e22c44469a0e3293
MrRocketR <kuptovns@gmail.com>
2022-03-22 22:39:15 +0300

    2. Статистика по коллекции

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -4,62 +4,30 @@ import java.util.*;
 
 public class Analize {
 
+
     public static Info diff(Set<User> previous, Set<User> current) {
-        Info result =  new Info(0, 0, 0);
-        int added = Analize.added(previous, current);
-        int changed = Analize.changed(previous, current);
-        int deleted = Analize.deleted(previous, current);
-        result.setAdded(added);
-        result.setChanged(changed);
-        result.setDeleted(deleted);
-        System.out.println(result.getChanged());
-        return result;
-
-    }
-
-    private static int added(Set<User> previous, Set<User> current) {
-       int out = 0;
-        ArrayList<Integer> idList = getIdList(previous);
-        for (User user : current) {
-            if (!idList.contains(user.getId())) {
-                out++;
-            }
-       }
-           return out;
-    }
-
-    private static int changed(Set<User> previous, Set<User> current) {
-        int out = 0;
+       int added = 0;
+       int changed = 0;
+       int deleted = 0;
+       int keysCount = 0;
         Map<Integer, String> list = new HashMap<>();
         for (User user : previous) {
             list.put(user.getId(), user.getName());
         }
         for (User user : current) {
             String temp = list.get(user.getId());
+            if (temp == null) {
+                added++;
+            }
             if (temp != null && !temp.equals(user.getName())) {
-                out++;
+                changed++;
+            }
+            if (list.containsKey(user.getId())) {
+                keysCount++;
             }
         }
-        return out;
+        deleted = previous.size() - keysCount;
+        return new Info(added, changed, deleted);
     }
 
-    private static int deleted(Set<User> previous, Set<User> current)  {
-        int out = 0;
-        ArrayList<Integer> idList = getIdList(current);
-        for (User user : previous) {
-            if (!idList.contains(user.getId())) {
-                out++;
-            }
-        }
-        return out;
-    }
-
-    private static ArrayList<Integer> getIdList(Set<User> userSet) {
-        ArrayList<Integer> idList = new ArrayList<>();
-        for (User user : userSet) {
-            int id = user.getId();
-            idList.add(id);
-        }
-        return idList;
-    }
 }
