e2f226cf726e7a71d86694109d97f76df50027b6
MrRocketR <kuptovns@gmail.com>
2022-03-31 20:59:38 +0300

    0.2. FileInputStream

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -4,13 +4,19 @@ import java.io.FileInputStream;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
+        StringBuilder s = new StringBuilder();
+        String[] lines;
         try (FileInputStream input = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
             int read;
-            int number;
+            int number = 0;
             while ((read = input.read()) != -1) {
-                Character myChar = (char) read;
-                number = Character.getNumericValue(myChar);
-                if (number % 2 == 0) {
+                text.append((char) read);
+                }
+            lines = text.toString().split("\\s+");
+            for (String line : lines) {
+                number = Integer.parseInt(line);
+                   if (number % 2 == 0) {
                     System.out.println("Number = " + number + " Is Even!");
                 }
             }
