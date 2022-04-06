987649913cbe122275b0fabb599793e1e6fc2745
MrRocketR <kuptsovns@gmail.com>
2022-04-06 23:02:59 +0300

    4.0. File

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -15,7 +15,6 @@ public class Dir {
         for (File subfile : file.listFiles()) {
             String s = String.format("%s : %x + Kb", subfile.getName(),subfile.length() / 1024);
             System.out.println(s);
-            //System.out.println(subfile.getName() + " : " + subfile.length() / 1024 + "Kb");
         }
     }
 }
