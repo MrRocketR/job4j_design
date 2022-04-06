4325416adbcf60a68a5f320fc7d9ef3aed351931
MrRocketR <kuptsovns@gmail.com>
2022-04-06 22:08:44 +0300

    0.3. BufferedReader

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -12,29 +12,18 @@ public class LogFilter {
             String line;
             while ((line = in.readLine()) != null) {
                 String[] arr = line.split("\\s");
-                if (arr[8].equals("404")) {
+                if ("404".equals(arr[8])) {
                     out.add(line);
                 }
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException io) {
+            io.printStackTrace();
         }
         return out;
     }
 
-    public static void save(List<String> log, String file) {
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(file)
-                ))) {
-            for (String s: log) {
-                out.println(s);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+
 
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
@@ -42,7 +31,7 @@ public class LogFilter {
         for (String test: log) {
             System.out.println(test);
         }
-       save(log, "404.txt");
+
 
     }
 
