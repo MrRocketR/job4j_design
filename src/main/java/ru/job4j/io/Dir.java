88b919b35454b8383d1d0a25e733dab6b2718158
MrRocketR <kuptsovns@gmail.com>
2022-04-06 23:02:41 +0300

    4.0. File

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("d:\\projects");
+        File file = new File("C:\\Nikita\\IT\\Java\\job4j_design");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
@@ -13,7 +13,9 @@ public class Dir {
         }
         System.out.println(String.format("size : %s", file.getTotalSpace()));
         for (File subfile : file.listFiles()) {
-            System.out.println(subfile.getName() + " : " + subfile.length() / 1024 + "Kb");
+            String s = String.format("%s : %x + Kb", subfile.getName(),subfile.length() / 1024);
+            System.out.println(s);
+            //System.out.println(subfile.getName() + " : " + subfile.length() / 1024 + "Kb");
         }
     }
 }
