d6238f2d5e0dde768b743df00961854a470e19b3
MrRocketR <kuptovns@gmail.com>
2022-04-17 17:20:38 +0300

    5.2. Архивировать проект

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -17,7 +17,7 @@ public class Zip {
     public void packFiles(List<File> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (File file : sources) {
-                if (!file.toString().contains(param2)) {
+                if (!file.getName().contains(param2)) {
                     zip.putNextEntry(new ZipEntry(file.getPath()));
                 }
             }
@@ -27,20 +27,25 @@ public class Zip {
     }
 
     public void argumentChecker(String[] arr) {
+        if (arr.length != 3) {
+            throw new IllegalArgumentException("Wrong number os arguments!");
+        }
         ArgsName argsName = ArgsName.of(arr);
         param1 = argsName.get("d");
         param2 = argsName.get("e");
         param3 = argsName.get("o");
+        if (!new File(param1).exists() && !new File(param1).isDirectory()) {
+            throw new IllegalArgumentException("Wrong directory!");
+        }
+        if (!param2.startsWith(".")) {
+            throw new IllegalArgumentException("Wrong filetype or filename extension");
+        }
+
     }
-
-
 
     public static void main(String[] args) {
         Zip zip = new Zip();
         zip.argumentChecker(args);
-        System.out.println(param1);
-        System.out.println(param2);
-        System.out.println(param3);
         File file = new File(param1);
         List<File> sources = new ArrayList<>(Arrays.asList((Objects.requireNonNull(file.listFiles()))));
         zip.packFiles(sources,  new File(param3));
