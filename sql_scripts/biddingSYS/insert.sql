4772e8bb3b51f38b9a95c4614f5ec098ab5e983c
MrRocketR <kuptovns@gmail.com>
2022-05-16 11:14:06 +0300

    1. Схема прав пользователи и роли

--- sql_scripts/biddingSYS/insert.sql
+++ sql_scripts/biddingSYS/insert.sql
@@ -1,8 +1,8 @@
-INSERT into role (name) value ('admin');
-INSERT into users (name, password, role_id) value ('admin', 'admin', 1);
-INSERT into rules (name) value ('rwd');
-INSERT into roleANDrules (name, rule_id, role_id) value ('main_rule', 1, 1);
-INSERT into category (name) value ('admin_category');
-INSERT into items (name, user_id, category_id, state_id) value ('0. Show all items', 1,1,1);
-INSERT into attachs (name, item_id) value ('Show',1);
-INSERT into comments (comm, item_id) value ('comment is ', 1);
+INSERT into role (name) values ('admin');
+INSERT into users (name, password, role_id) values ('admin', 'admin', 1);
+INSERT into rules (name) values ('rwd');
+INSERT into roleANDrules (name, rule_id, role_id) values ('main_rule', 1, 1);
+INSERT into category (name) values ('admin_category');
+INSERT into items (name, user_id, category_id, state_id) values ('0. Show all items', 1,1,1);
+INSERT into attachs (name, item_id) values ('Show',1);
+INSERT into comments (comm, item_id) values ('comment is ', 1);
